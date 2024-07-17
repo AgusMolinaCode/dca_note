6b3abe3eb66af7e38ccd32ef21aa9978561af300
Agustin Molina <am.motos@hotmail.com>
2024-07-17 02:03:40 -0300

    chore: Refactor CurrentBalance component, add CurrentBalanceItem subcomponent, and update API endpoint for adding values

--- components/dashboard/CurrentBalance.tsx
+++ components/dashboard/CurrentBalance.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
 import React, { useEffect, useState } from "react";
@@ -5,12 +6,15 @@ import { useQuery } from "@tanstack/react-query";
 import { loadTransactions, getMultipleCryptos } from "@/app/api";
 import CurrentBalanceItem from "./CurrentBalanceItem";
 import { useUser } from "@clerk/clerk-react";
+import { Button } from "../ui/button";
 
 type CryptoPrices = {
   [key: string]: {
     USD: number;
   };
 };
+
+const ADD_VALUES_URL = `http://localhost:3000/api/values`;
 
 const CurrentBalance = () => {
   const [cryptoPrices, setCryptoPrices] = useState<CryptoPrices>({});
@@ -24,9 +28,7 @@ const CurrentBalance = () => {
   const { data } = useQuery({
     queryKey: ["items"],
     queryFn: loadTransactions,
-    refetchInterval(query) {
-      return 36000000;
-    },
+    refetchInterval: 1800000,
   });
 
   const dataUserId = data?.filter((item) => item.userId === user?.id);
@@ -78,11 +80,39 @@ const CurrentBalance = () => {
     setTotalSum(sum);
   }, [dataUserId]);
 
-  useEffect(() => {
-      setTotalProfitValue(totalValue);
-  }, [totalValue]);
+  const postData = async () => {
+    const valueData = {
+      userId: user?.id,
+      total: totalValue,
+    };
 
-  console.log(totalProfitValue);
+    try {
+      const response = await fetch(ADD_VALUES_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(valueData),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log("Respuesta del servidor:", data);
+    } catch (error) {
+      console.error("Error al realizar la solicitud POST:", error);
+    }
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      postData();
+    }, 1800000); // 60000 ms = 1 minuto
+
+    return () => clearInterval(timer); 
+  }, [postData]);
 
   const formattedTotalSum = totalSum.toLocaleString("en-US", {
     style: "currency",
@@ -101,11 +131,13 @@ const CurrentBalance = () => {
       currency: "USD",
     });
 
+
   return (
     <div className="">
       <div className="flex justify-between items-center">
         <h1 className="text-lg font-semibold text-gray-500">Current Balance</h1>
         <p className="text-gray-500 text-md font-semibold">24h</p>
+       
       </div>
       <div>
         <p className="text-4xl font-semibold pt-4">{formattedTotalValue}</p>
