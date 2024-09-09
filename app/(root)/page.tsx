b19170532942aa62a448bbc838ab28a2fc145c2b
Agustin Molina <am.motos@hotmail.com>
2024-09-08 23:46:07 -0300

    Refactorizar estilos de la tabla de transacciones y agregar lazy loading para imágenes y actualizar diseño del pie de página

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,8 @@
-import Footer from "@/components/shared/Footer";
 import { Hero } from "@/components/shared/Hero";
 import InfoSection from "@/components/shared/InfoSection";
-
+import { TextHoverEffect } from "@/components/ui/text-hover-effect";
+import { Github, Linkedin } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,15 +10,30 @@ export default function Home() {
       <div className=" w-full dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-        <div className="relative z-20 ">
-          <Hero />
-          <InfoSection />
-          <p className="dark:text-stone-900/50 text-stone-200/50 text-center font-bold text-[7rem] md:text-[10rem] lg:text-[22rem] z-30 mb-10 lg:mb-0">
-            HODL 
-          </p>
-          <div className="flex justify-center items-center mx-auto">
-            <Footer />
-          </div>
+
+        <Hero />
+        <InfoSection />
+
+        {/* <Footer /> */}
+        <div className="h-[30rem] hidden md:flex items-center justify-center">
+          <TextHoverEffect text="HODL" />
+        </div>
+        <div className="flex justify-center gap-4 pb-5 mt-8 md:mt-0 md:pb-10">
+          <Link
+            href="https://www.linkedin.com/in/agustin-molina-994635138/"
+            target="_blank"
+          >
+            <Linkedin
+              size={32}
+              className="text-neutral-600 dark:text-neutral-400 hover:text-blue-500 dark:hover:text-blue-500 duration-200"
+            />
+          </Link>
+          <Link href="https://github.com/AgusMolinaCode" target="_blank">
+            <Github
+              size={32}
+              className="text-neutral-600 dark:text-neutral-400 hover:text-blue-500 dark:hover:text-blue-500 duration-200"
+            />
+          </Link>
         </div>
       </div>
     </div>
